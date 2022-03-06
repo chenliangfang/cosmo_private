46d897e93fd1b32f06583a3959d660f26e696080
chenlf <cosmoivy@126.com>
2022-03-06 22:14:19 +0800

    wechat_test

---
+++ wechat_test.py
@@ -0,0 +1,8 @@
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
+
+try:
+    check_signature(token, signature, timestamp, nonce)
+except InvalidSignatureException:
+    pass
+    # 处理异常情况或忽略
